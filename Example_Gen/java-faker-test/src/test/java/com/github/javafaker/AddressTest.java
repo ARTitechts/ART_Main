9a04103850f5924269283930114377b92a43a691
manishgant <manishgant@gmail.com>
2015-01-19 23:06:58 +0530

    Added Example Gen

---
+++ Example_Gen/java-faker-test/src/test/java/com/github/javafaker/AddressTest.java
@@ -0,0 +1,60 @@
+package com.github.javafaker;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.github.javafaker.matchers.IsANumber.isANumber;
+import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AddressTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(AddressTest.class);
+    private Faker faker;
+
+    @Before
+    public void before() {
+        faker = new Faker();
+    }
+
+    @Test
+    public void testStreetAddressIsANumber() {
+        final String streetAddressNumber = faker.address().streetAddressNumber();
+        logger.info("Street Address Number: " + streetAddressNumber);
+        assertThat(streetAddressNumber, isANumber());
+    }
+
+    @Test
+    public void testLatitude() {
+        String latStr;
+        Double lat;
+        for (int i = 0; i < 100; i++) {
+            latStr = faker.address().latitude();
+            assertThat(latStr, isANumber());
+            lat = new Double(latStr);
+            assertThat("Latitude is less then -90", lat >= -90);
+            assertThat("Latitude is greater than 90", lat <= 90);
+        }
+    }
+
+    @Test
+    public void testLongitude() {
+        String longStr;
+        Double lon;
+        for (int i = 0; i < 100; i++) {
+            longStr = faker.address().longitude();
+            assertThat(longStr, isANumber());
+            lon = new Double(longStr);
+            assertThat("Longitude is less then -180", lon >= -180);
+            assertThat("Longitude is greater than 180", lon <= 180);
+        }
+    }
+
+
+    @Test
+    public void testTimeZone() {
+        assertThat(faker.address().timeZone(), matchesRegularExpression("[A-Za-z_]+/[A-Za-z_]+[/A-Za-z_]*"));
+    }
+}
